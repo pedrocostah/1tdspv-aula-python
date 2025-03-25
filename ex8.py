997b704869975295baf00d5eaecb7aea7c399c3f
pedrocostah <pedrocostahv@gmail.com>
2025-03-25 18:46:08 -0300

    arquivo adicionado

---
+++ ex8.py
@@ -0,0 +1,7 @@
+x = int(input("Digite o numero de camisas: "))
+y = int(input("Digite o numero de calcas: "))
+z = int(input("Digite o numero de sapatos: "))
+
+numero_combinacoes = x * y * z 
+
+print("O numero de combinacoes possiveis e ", numero_combinacoes)
